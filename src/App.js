158945f70633ecfa049f1dc4dcb02a7e5a7f6a3f
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-21 15:27:17 +0300

    create Footer component + export

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from "./Header";
 import Tasks from "./Tasks";
+import Footer from "./Footer";
 
 class  App extends React.Component {
   render =()=> {
@@ -9,6 +10,7 @@ class  App extends React.Component {
         <div>
           <Header/>
           <Tasks/>
+          <Footer/>
 
         </div>
     );
