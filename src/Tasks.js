0e1e3026526c53deb6d2eae1723758e04bbeb94f
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-21 15:32:52 +0300

    add JSX elements to Tasks component

--- src/Tasks.js
+++ src/Tasks.js
@@ -5,6 +5,22 @@ class  Tasks extends React.Component {
   render =()=> {
     return (
         <div>
+            <div>
+                <input type='checkbox' checked={true}/>
+                <span>HTML</span>
+            </div>
+            <div>
+                <input type='checkbox' checked={true}/>
+                <span>CSS</span>
+            </div>
+            <div>
+                <input type='checkbox' checked={true}/>
+                <span>JS</span>
+            </div>
+            <div>
+                <input type='checkbox' checked={true}/>
+                <span>REACT</span>
+            </div>
 
         </div>
     );
