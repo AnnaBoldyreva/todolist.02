545aa83d49bd160cffa26d621c0a81f9810fc664
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-21 15:26:28 +0300

    create Tasks component + export

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from "./Header";
+import Tasks from "./Tasks";
 
 class  App extends React.Component {
   render =()=> {
     return (
         <div>
           <Header/>
+          <Tasks/>
 
         </div>
     );
---
+++ src/Tasks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class  Tasks extends React.Component {
+  render =()=> {
+    return (
+        <div>
+
+        </div>
+    );
+  }
+}
+
+export default Tasks;
