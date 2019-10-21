adf30d8197990ec430b11cf88c9255c68ca01af2
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-21 15:30:57 +0300

    add JSX elements to Header component

--- src/Footer.js
+++ src/Footer.js
@@ -6,6 +6,7 @@ class  Footer extends React.Component {
     return (
         <div>
 
+
         </div>
     );
   }
--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,11 @@ class  Header extends React.Component {
   render =()=> {
     return (
         <div>
-
+            <h3>What to read?</h3>
+            <div>
+                <input type='text' placeholder='new book name'/>
+                <button>Add</button>
+            </div>
         </div>
     );
   }
