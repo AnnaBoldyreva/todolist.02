3a236fcbfade3e5b4bea170f7226e4382ade53e7
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-24 16:04:53 +0300

    add method for hide and show span

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 
 class  Footer extends React.Component {
-  render =()=> {
+    state = {
+        isHidden: false
+    };
+    onAllFilterClick = () => {};
+    onCompletedFilterClick = () => {};
+    onActiveFilterClick = () => {};
+    onShowFiltersClick = () => {
+        this.setState({isHidden : true})
+    };
+    onHideFiltersClick = () => {this.setState({isHidden: false})};
+
+    render =()=> {
       let classForAll = this.props.filterValue === 'All' ? 'filterActive': ' ' ;
       let classForCompleted = this.props.filterValue === 'Completed' ? 'filterActive': ' ' ;
       let classForActive = this.props.filterValue === 'Active' ? 'filterActive': ' ' ;
@@ -11,8 +22,9 @@ class  Footer extends React.Component {
             <button onClick={()=> {this.props.changeFilter('All')}} className={classForAll }>All</button>
             <button onClick={()=> {this.props.changeFilter('Completed')}} className={classForCompleted}>Completed</button>
             <button onClick={()=> {this.props.changeFilter('Active')}} className={classForActive}>Active</button>
-            <span>hide</span>
-            <span>show</span>
+           <br/>
+            {!this.state.isHidden && <span onClick={this.onShowFiltersClick}>hide</span>}
+            {this.state.isHidden &&<span onClick={this.onHideFiltersClick}>show</span>}
         </div>
     );
   }
