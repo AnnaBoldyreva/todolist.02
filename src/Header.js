503f521bcbf283454be9679dee2e6d537e0052f8
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-24 17:52:12 +0300

    add styles for error to newText

--- src/Header.js
+++ src/Header.js
@@ -5,19 +5,25 @@ class  Header extends React.Component {
     newBookTitleRef = React.createRef();
     onAddBookClick = () => {
         let newText = this.newBookTitleRef.current.value;
+        if (newText === ''){
+            this.setState({error : true})
+        } else {
+            this.setState({error : false})
+        }
         this.newBookTitleRef.current.value = '';
         this.props.addBook(newText)
     };
 
     state = {
-        error : true
+        error : false
     };
   render =()=> {
+      let errorFilter = this.state.error ? 'error' : '';
     return (
         <div>
             <h3>What to read?</h3>
             <div>
-                <input ref={this.newBookTitleRef} type='text' placeholder='new book name' className='error'/>
+                <input ref={this.newBookTitleRef} type='text' placeholder='new book name' className={errorFilter}/>
                 <button onClick={this.onAddBookClick}>Add</button>
             </div>
         </div>
