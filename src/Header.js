7419939ceb3f3d9339c8eeedae1c5b70034ea868
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-24 17:46:43 +0300

    add state for header

--- src/Header.js
+++ src/Header.js
@@ -8,12 +8,16 @@ class  Header extends React.Component {
         this.newBookTitleRef.current.value = '';
         this.props.addBook(newText)
     };
+
+    state = {
+        error : true
+    };
   render =()=> {
     return (
         <div>
             <h3>What to read?</h3>
             <div>
-                <input ref={this.newBookTitleRef} type='text' placeholder='new book name'/>
+                <input ref={this.newBookTitleRef} type='text' placeholder='new book name' className='error'/>
                 <button onClick={this.onAddBookClick}>Add</button>
             </div>
         </div>
