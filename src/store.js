107bf17b2d181cebdaa402b0118d51de19506e59
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-11-29 19:55:33 +0300

    add data from our first step, use localStorage

--- src/store.js
+++ src/store.js
@@ -2,7 +2,11 @@ import {createStore} from "redux";
 
 
 const initialState = {
-    todoLists : []
+    todoLists : [
+        {title: "to eat", id: 0},
+        {title: "to do", id: 1},
+        {title: "to watch", id: 2}
+    ]
 };
 
 const reducer = (state = initialState, action) => {
